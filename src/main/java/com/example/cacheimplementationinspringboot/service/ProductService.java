943bc45d20f3c79ded5e5e086fc562d2430d7770
Ravi Kumar Gupta <babaravibbb@gmail.com>
2023-12-21 11:26:26 +0530

    Cache Implementation

---
+++ src/main/java/com/example/cacheimplementationinspringboot/service/ProductService.java
@@ -0,0 +1,12 @@
+package com.example.cacheimplementationinspringboot.service;
+
+import com.example.cacheimplementationinspringboot.entity.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> getAllProducts();
+
+    Product insertProduct(Product product);
+}
